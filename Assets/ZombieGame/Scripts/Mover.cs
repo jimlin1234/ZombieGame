c3956f419586e5d5bf2555f0650128345ae508f5
Jim <jimlin0914@gmail.com>
2025-07-17 15:02:46 +0800

    MoveToCorsor()

--- Assets/ZombieGame/Scripts/Mover.cs
+++ Assets/ZombieGame/Scripts/Mover.cs
@@ -6,12 +6,31 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
-   
+    //Ray lastRay;
+
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCorsor();
+        }
+        
     }
+        
+    private void MoveToCorsor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        //Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+        bool hasHit = Physics.Raycast(ray,out hit);
+        if(hasHit == true)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+       // lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //GetComponent<NavMeshAgent>().destination = target.position;
+    }
+    
 }
