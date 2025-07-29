0d9a6eb66b5cefefd902f51fb0b2c99a93985fd4
Jim <jimlin0914@gmail.com>
2025-07-29 10:59:58 +0800

    Update ActionScheduler.cs

--- Assets/RPG/Scripts/Core/ActionScheduler.cs
+++ Assets/RPG/Scripts/Core/ActionScheduler.cs
@@ -7,7 +7,7 @@ namespace RPG.Core
     public class ActionScheduler : MonoBehaviour
     {
         IAction currentAction; //目前的動作
-        public void StartAction(IAction action) //因為Fighter 和 Mover 皆繼承了MonoBehaviour
+        public void StartAction(IAction action) //因為Fighter 和 Mover 皆繼承了IAction  介面
         {
             //print(action);
             if (currentAction == action) return; //如果都是一樣的就直接跳出，如果不一樣(切換了action)就執行下去
