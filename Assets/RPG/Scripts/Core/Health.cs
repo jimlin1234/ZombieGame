552523baf73730950321e15f6996330ec9399914
Jim <36058153+jimlin1234@users.noreply.github.com>
2025-08-02 19:34:30 +0800

    Health

--- Assets/RPG/Scripts/Core/Health.cs
+++ Assets/RPG/Scripts/Core/Health.cs
@@ -1,8 +1,9 @@
+using RPG.Movement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace RPG.Combat
+namespace RPG.Core
 {
     public class Health : MonoBehaviour
     {
