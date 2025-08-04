8b885ce1d18df918cae14e16ce533659dcfffa4c
Jim <jimlin0914@gmail.com>
2025-08-04 18:41:51 +0800

    註解

--- Assets/RPG/Scripts/Core/ActionScheduler.cs
+++ Assets/RPG/Scripts/Core/ActionScheduler.cs
@@ -13,7 +13,7 @@ namespace RPG.Core
             if (currentAction == action) return; //如果都是一樣的就直接跳出，如果不一樣(切換了action)就執行下去
             if (currentAction != null)
             {
-                currentAction.Cancel();
+                currentAction.Cancel(); //Mover 及 Fighter都有繼承IAction 介面，所以都要實作此方法，依照傳入的IAction物件不同，去執行自己的Cancel方法
             }
             currentAction = action;
             
--- Assets/RPG/Scripts/Core/IAction.cs
+++ Assets/RPG/Scripts/Core/IAction.cs
@@ -1,3 +1,4 @@
+using RPG.Movement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,6 @@ namespace RPG.Core
 {
     public interface IAction
     {
-        public void Cancel();
+        public void Cancel(); //Mover 及 Fighter都有繼承，所以都要實作此方法
     }
 }
