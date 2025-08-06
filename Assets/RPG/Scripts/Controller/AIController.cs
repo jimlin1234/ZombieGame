f3398575305ddc6ee4daaad6956807ac10ad309b
Jim <jimlin0914@gmail.com>
2025-08-06 15:26:52 +0800

    Update AIController.cs

--- Assets/RPG/Scripts/Controller/AIController.cs
+++ Assets/RPG/Scripts/Controller/AIController.cs
@@ -19,7 +19,7 @@ namespace RPG.Controller
         Health health;
 
         Vector3 guardPosition; //敵人原本位置
-        float timeSinceLastSawPlayer = Mathf.Infinity; //敵人上次最後追擊player的時間
+        float timeSinceLastSawPlayer = Mathf.Infinity; //敵人上次最後追擊(看見)player的時間
 
         private void Start()
         {
