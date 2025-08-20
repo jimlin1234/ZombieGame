aad716326caa8f4cbf393a439ad792bf0b51dc79
Jim <jimlin0914@gmail.com>
2025-08-20 19:03:39 +0800

    load scene

---
+++ Assets/RPG/Scripts/Core/Portal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.Core
+{
+    public class Portal : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene("SandBox2");
+            }
+            
+        }
+    }
+}
